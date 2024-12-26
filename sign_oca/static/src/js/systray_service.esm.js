5541670ba30d9a4eb503952b11831d8f05603b5d
kobros-tech <alkobroslymohamed@gmail.com>
2024-12-26 15:51:57 +0300

    [18.0][MIG] sign_oca: Migration to 18.0

--- sign_oca/static/src/js/systray_service.esm.js
+++ sign_oca/static/src/js/systray_service.esm.js
@@ -1,31 +1,27 @@
 /* @odoo-module */
-import {Component, useState} from "@odoo/owl";
+/* global document */
+import {Component, onMounted, useState} from "@odoo/owl";
 import {Dropdown} from "@web/core/dropdown/dropdown";
 import {DropdownItem} from "@web/core/dropdown/dropdown_item";
 import {registry} from "@web/core/registry";
-import {session} from "@web/session";
 import {useDiscussSystray} from "@mail/utils/common/hooks";
 import {useService} from "@web/core/utils/hooks";
+import {user} from "@web/core/user";
+import {_t} from "@web/core/l10n/translation";
+
 const systrayRegistry = registry.category("systray");
 export class SignerMenuView extends Component {
     setup() {
         this.discussSystray = useDiscussSystray();
         this.orm = useService("orm");
-        this.store = useState(useService("mail.store"));
         this.action = useService("action");
-        this.fetchSystraySigner();
-    }
-    async fetchSystraySigner() {
-        const groups = await this.orm.call("res.users", "sign_oca_request_user_count");
-        let total = 0;
-        for (const group of groups) {
-            total += group.total_records || 0;
-        }
-        this.store.signerCounter = total;
-        this.store.signerGroups = groups;
-    }
-    onBeforeOpen() {
-        this.fetchSystraySigner();
+        this.watchSignRequests = useService("watchSignRequests");
+        this.state = useState({
+            sign_requests: this.watchSignRequests.sign_requests,
+        });
+        onMounted(async () => {
+            await this.watchSignRequests.fetchSystraySigner();
+        });
     }
     availableViews() {
         return [
@@ -35,24 +31,29 @@ export class SignerMenuView extends Component {
             [false, "activity"],
         ];
     }
-    onClickFilterButton(group) {
-        document.body.click(); // Hack to close dropdown
+    async onClickFilterButton(group) {
+        // Hack to close dropdown
+        document.body.click();
         const context = {};
         const views = this.availableViews();
-        var domain = [
-            ["request_id.state", "=", "sent"],
-            ["partner_id", "child_of", [session.partner_id]],
-            ["signed_on", "=", false],
-        ];
+        // Partner_id is removed from session in version 18.0
+        // Importing user object which is the current user instead of session object
         this.action.doAction(
             {
-                context,
-                domain,
-                name: group.name,
-                res_model: "sign.oca.request.signer",
-                search_view_id: [false],
                 type: "ir.actions.act_window",
+                name:
+                    group && group.name && group.name !== "Undefined"
+                        ? _t(group.name)
+                        : _t("Documents to be Signed"),
+                res_model: "sign.oca.request.signer",
                 views,
+                search_view_id: [false],
+                domain: [
+                    ["request_id.state", "=", "sent"],
+                    ["partner_id", "child_of", [user.partnerId]],
+                    ["signed_on", "=", false],
+                ],
+                context,
             },
             {
                 clearBreadcrumbs: true,
